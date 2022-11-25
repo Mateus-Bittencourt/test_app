46951fdb5ab266795a0438ef0702180c3caf60cf
Mateus Bittencourt <mateusmbittencourt@gmail.com>
2022-11-25 17:10:55 -0300

    add product model specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,7 +30,7 @@ RSpec.configure do |config|
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
 
-  config.order = :random
+  # config.order = :random
 
   # FactoryBot Lint
   config.before(:suite) do
